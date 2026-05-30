cf4574f10410d810fc57548fb99a15973787632c
lexaiko <ekosusantobagussusanto@gmail.com>
2026-05-30 14:02:30 +0700

    Change main couple image from png to webp

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -17,7 +17,7 @@ export const data = {
             image: './src/assets/images/cewe.png'
         },
 
-        couple: './src/assets/images/couple.png'
+        couple: './src/assets/images/couple.webp'
     },
 
     time: {
